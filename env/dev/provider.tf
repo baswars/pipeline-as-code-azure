468e4416432df0a3d48208e104dfd1889a5c1a6e
sachin B <sachin.b@gmail.com>
2024-08-09 19:40:50 +0530

    backend-code-added

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "3.114.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "rg-devops"
+    storage_account_name = "storageaccountdevops"
+    container_name = "container-devops"
+    key = "terraform.dev.tfstate"
+    
+  }
 }
 provider "azurerm" {
   features {}
